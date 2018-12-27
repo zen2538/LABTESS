4d27265e7c417c1733606148e76c2ca4799aa310
C4psL0cks <fenixaline@hotmail.com>
2018-12-27 22:36:30 +0700

    mz

--- verify.php
+++ verify.php
@@ -3,15 +3,23 @@ $access_token = "eAnt4bRuZ7yYDz0JTDKUhbQp0slqwUHMepuTGOfbhZwJurCrd7y8qXjX8UaST8x
 
 $url = 'https://api.line.me/v2/oauth/verify';
 
-$headers = array('Content-Type : application/x-www-form-urlencoded');
-$body = array('access_token'. $access_token);
-
+// $headers = array('Content-Type : application/x-www-form-urlencoded');
+// $body = array('access_token'. $access_token);
 
 $ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $body);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+// curl_setopt($ch, CURLOPT_HTTPBODAY, $body);
+// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+curl_setopt($ch, CURLOPT_POST,           1 );
+curl_setopt($ch, CURLOPT_POSTFIELDS,     $access_token); 
+curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Content-Type: application/x-www-form-urlencoded')); 
+
+
 $result = curl_exec($ch);
 curl_close($ch);
 
